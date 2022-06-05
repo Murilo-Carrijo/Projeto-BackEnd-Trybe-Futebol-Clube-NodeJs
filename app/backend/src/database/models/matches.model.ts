7b9ee1f7784080ca17029d03afb8b4c3f8b7f5b6
murilo <murilocarrijoadm@gmail.com>
2022-06-05 11:23:03 -0300

    feat: Model para a rota Matches criada com as devidas Associações

---
+++ app/backend/src/database/models/matches.model.ts
@@ -0,0 +1,52 @@
+import { Model, INTEGER } from 'sequelize';
+import db from '.';
+import Teams from './temas.model';
+
+class Matches extends Model {
+  id!: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: number;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'metches',
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+Teams.hasMany(Matches, { foreignKey: 'id', as: 'teamId' });
+
+export default Matches;
